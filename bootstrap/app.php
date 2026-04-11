29ea29467aa02d4137b9331b4c9827dfdcab8e9d
Deepanshu9045 <rdeepanshu170@gmail.com>
2026-04-11 23:42:29 +0530

    redeployed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -32,7 +32,7 @@ foreach ($runtimeDefaults as $key => $value) {
     }
 }
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -44,3 +44,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+$app->dontMergeFrameworkConfiguration();
+
+return $app;
